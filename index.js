e8ffbf4b9ffee66ec929544d55ffb536a8cd0d16
Abhinav Awana <abhinavawana2444@gmail.com>
2024-06-09 16:59:21 +0530

    change the step traversal technique and step one style

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-let nxt = document.querySelectorAll(".next");
-let back= document.querySelectorAll(".back");
-let confirm = document.querySelector(".confirm");
+let nxt = document.querySelector(".next");
 
-let x=0;
-let y=0;
+let x=1;
 
 function indicator(val1=0, val2=0){
     if(val1){
@@ -17,28 +14,49 @@ function indicator(val1=0, val2=0){
     
 }
 
-nxt.forEach(btn =>{
-    btn.addEventListener("click",function(){
-        x=parseInt(btn.value);
-        document.querySelector("#step-"+x).classList.toggle("hidden");
-        indicator(x,0);
-        x=x+1;
-        document.querySelector("#step-"+x).classList.toggle("hidden");
-        
-    });
-});
 
-back.forEach(btn => {
-    btn.addEventListener("click",function(){
-        y =parseInt(btn.value);
-        let prev=y+1
-        document.querySelector("#step-"+prev).classList.toggle("hidden");
-        document.querySelector("#step-"+y).classList.toggle("hidden");
-        indicator(0,y);
-    });
-});
 
-confirm.addEventListener("click",function(){
+function conf(){
     document.querySelector("#step-4").classList.toggle("hidden");
     document.querySelector("#step-5").classList.toggle("hidden");
+    document.querySelector(".pg-control").style.display = "none";
+}
+
+nxt.addEventListener("click",function(){
+    document.querySelector("#step-"+x).classList.toggle("hidden");
+    
+    indicator(x,0);
+    x=x+1;
+    if(x>1){
+        document.querySelector(".back").classList.remove("hidden")
+    }
+    document.querySelector("#step-"+x).classList.toggle("hidden");
+    if(x===4){
+        document.querySelector(".confirm").classList.remove("hidden");
+        nxt.classList.add("hidden");
+    }
+    
 });
+
+function back(){
+
+    document.querySelector("#step-"+x).classList.toggle("hidden");
+    x=x-1
+    document.querySelector("#step-"+x).classList.toggle("hidden");
+    if(x<2){
+        document.querySelector(".back").classList.add("hidden")
+    }
+    if(x!==4){
+        document.querySelector(".confirm").classList.add("hidden");
+        nxt.classList.remove("hidden");
+    }
+    indicator(0,x);
+}
+// back.addEventListener("click",function(){
+//     let prev=y+1
+//     document.querySelector("#step-"+prev).classList.toggle("hidden");
+//     document.querySelector("#step-"+y).classList.toggle("hidden");
+//     indicator(0,y);
+// });
+
+
