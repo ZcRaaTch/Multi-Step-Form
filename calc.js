5b037ef2b3d65b68ebd38b00499a061712ceaab4
Abhinav Awana <abhinavawana2444@gmail.com>
2024-06-20 10:40:38 +0530

    changed calc.js for step3

--- calc.js
+++ calc.js
@@ -1,6 +1,7 @@
 let card = document.querySelectorAll(".card");
 let monthly = document.querySelector(".monthly");
 let yearly = document.querySelector(".yearly");
+let addOnCard = document.querySelectorAll(".sec-card");
 let onsvcs = document.querySelector(".online-service");
 let lrgstrg = document.querySelector(".larger-storage");
 let customizable = document.querySelector(".customizable");
@@ -18,7 +19,11 @@ let varRate = document.querySelectorAll(".variable-rate");
 // defining variable
 var varRateMonthly = ["9/mo", "12/mo", "15/mo"];
 var varRateYearly = ["90/yr", "120/yr", "150/yr"];
-var planValue = 0;
+var planValue = 9;
+var addOnValue = 0;
+var totalAmount = 0;
+
+card[0].classList.add("selected-plan");
 // functions
 function changeValue(arr1) {
   for (var i = 0; i < 3; i++) {
@@ -30,23 +35,32 @@ function changePlanValue(val) {
     card[i].dataset.value = val[i];
   }
 }
+function changeAddonValue(val) {
+  for (var i = 0; i < 3; i++) {
+    addOnCard[i].dataset.value = val[i];
+  }
+}
 function planReset() {
-  planValue = 0;
+  planValue = card[0].dataset.value;
   for (var i = 0; i < 3; i++) {
     card[i].classList.remove("selected-plan");
   }
+  card[0].classList.add("selected-plan");
+  for (var i = 0; i < 3; i++) {
+    addOnCard[i].classList.remove("selected-plan");
+  }
+  addOnValue = 0;
 }
 // event listeners
 card.forEach((btn) => {
   btn.addEventListener("click", function () {
     planValue = btn.dataset.value;
-    console.log(planValue);
     for (var i = 0; i < 3; i++) {
       if (card[i].dataset.value != planValue) {
         card[i].classList.remove("selected-plan");
       }
     }
-    btn.classList.toggle("selected-plan");
+    btn.classList.add("selected-plan");
   });
 });
 monthly.addEventListener("click", function () {
@@ -57,6 +71,7 @@ monthly.addEventListener("click", function () {
   checker.classList.remove("year");
   changeValue(varRateMonthly);
   changePlanValue([9, 12, 15]);
+  changeAddonValue([1, 2, 2]);
   planReset();
 });
 yearly.addEventListener("click", function () {
@@ -67,5 +82,35 @@ yearly.addEventListener("click", function () {
   checker.classList.remove("month");
   changeValue(varRateYearly);
   changePlanValue([90, 120, 150]);
+  changeAddonValue([10, 20, 20]);
   planReset();
+});
+onsvcs.addEventListener("click", function () {
+  ossvg.classList.toggle("svg-bg");
+  onsvcs.classList.toggle("selected-plan");
+  if (onsvcs.classList.contains("selected-plan")) {
+    addOnValue += onsvcs.dataset.addon;
+  } else {
+    addOnValue -= onsvcs.dataset.addon;
+  }
+
+  console.log();
+});
+lrgstrg.addEventListener("click", function () {
+  lssvg.classList.toggle("svg-bg");
+  lrgstrg.classList.toggle("selected-plan");
+  if (onsvcs.classList.contains("selected-plan")) {
+    addOnValue += lrgstrg.dataset.addon;
+  } else {
+    addOnValue -= lrgstrg.dataset.addon;
+  }
+});
+customizable.addEventListener("click", function () {
+  csvg.classList.toggle("svg-bg");
+  customizable.classList.toggle("selected-plan");
+  if (onsvcs.classList.contains("selected-plan")) {
+    addOnValue += customizable.dataset.addon;
+  } else {
+    addOnValue -= customizable.dataset.addon;
+  }
 });
